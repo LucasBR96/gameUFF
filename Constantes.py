8c1be9552dad4d625824e14529c66a776008ea43
Lucas Fuzato Cipriano <36990809+LucasBR96@users.noreply.github.com>
2019-10-14 22:07:38 -0300

    Update Constantes.py

--- Constantes.py
+++ Constantes.py
@@ -1,1 +1,25 @@
+#Constantes de física( considerando um metro = 100 pixels )
+G = 980 # Aceleração da gravidade
 
+#constantes do player--------------------------------------------------------------
+VELPLAYERPULO = -120 #velocidade de pulo do player
+VELANDARPLAYER = 25
+VELCORRERPLAYER = 60
+VXUPGRADE = 10
+
+
+#Flags-------------------------------------------------------------------------------
+#colisão
+DECIMA =  0 #Colisão com self vindo de cima
+DEBAIXO = 1
+DAESQ =   2
+DADIR =   3
+
+#direção de movimento
+ESQ = -1 
+DIR = 1
+
+#movimento escolhido
+PUL = 1
+CORR = 2
+AND = 3
